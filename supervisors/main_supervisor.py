c71c70e3bdb310fb596a1c572e644faf03abebbf
Francisco Kovacevich <frankovacevich@gmail.com>
2021-08-12 09:21:28 -0300

    Cleaned up the code

--- supervisors/main_supervisor.py
+++ supervisors/main_supervisor.py
@@ -2,38 +2,31 @@
 Saves process data to database and forwards messages to upstream server
 """
 
-import json
 import datetime
 import traceback
-import sys
-
 from common.bin.logger import Log
 from common.bin.namespace_manager import NamespaceManager
 from common.lib.custom_mqtt_connection import custom_mqtt_connection
 
 # Create log
 log = Log("main_supervisor.txt")
-
 # Create mqtt client
 mqtt_connection = custom_mqtt_connection("main_supervisor")
-
 # Namespace Manager
 namespace_manager = NamespaceManager()
 
-def process_new_message(topic, message):
+
+def process_new_message(topic, data):
     # Get message
     try:
-        data = message
-        if not isinstance(data, dict):
-            raise Exception("Badly formatted data (data must be a dict)")
+        if not isinstance(data, dict): raise Exception("Badly formatted data (data must be a dict)")
     except Exception as e:
         error_as_string = ''.join(traceback.format_exception(None, e, e.__traceback__))
         log.write("Error while trying to decode data. " + str(e) + "\n\n" + error_as_string)
         return
 
     # Add t if not present
-    if "t" not in data:
-        data["t"] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    if "t" not in data: data["t"] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
     # Save to database
     if "__test__" in data: return
