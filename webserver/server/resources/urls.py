607fe828d729b237d9e8065c57973244f83cab29
Francisco Kovacevich <frankovacevich@gmail.com>
2021-09-28 08:37:45 -0300

    Changed folder structure

--- webserver/server/resources/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-	#path('<str:resource_type>/<str:resource_id>', views.resource, name='resource'),
-	#path('<str:resource_type>/<str:resource_id>/<str:date>', views.resource, name='resource'),
-	path('<path:resource_path>', views.res_, name='resource'),
-]
