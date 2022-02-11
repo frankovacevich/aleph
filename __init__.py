207f676fb702841d7e8f14fe7bfa229f0197b51d
Francisco Kovacevich <fran.kovacevich@gmail.com>
2022-02-11 18:35:14 -0300

    Separated branches

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .common import *
-from .connections import *
-from .connections.connection import Connection
-from .connections.namespace_connection import NamespaceConnection
-from .services.service import Service
