2e9c05eb049ad89faf7526f55f2462de41213fbb
Francisco Kovacevich <fran.kovacevich@gmail.com>
2022-02-15 06:39:44 -0300

    Fixed bugs

--- common/database_field_parse.py
+++ common/database_field_parse.py
@@ -1,12 +1,21 @@
 
 
 def db_parse_key(key):
-    return key.replace(".", "_").replace("/", "_")
+    return key.replace("/", ".").replace(".", "_")
 
 
 def db_parse_field(field):
-    return field.replace(".", "_")
+    return field.replace("/", ".").replace(".", "__")
 
 
 def db_deparse_field(field):
-    return field
+    return field.replace("__", ".")
+
+
+def db_parse_record_fields(record):
+    return {f: db_parse_field(record[f]) for f in record}
+
+
+def db_deparse_record_fields(record):
+    return {f: db_deparse_field(record[f]) for f in record}
+
