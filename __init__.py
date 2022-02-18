0b46f5bb65b91eb95f1829a853b1ac95f5aa345c
Your Name <you@example.com>
2022-02-18 05:09:39 -0300

    Fixed references

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .common import *
 from .connections import *
 from .connections.connection import Connection
-from .connections.namespace_connection import NamespaceConnection
+from .connections.mqtt_namespace_connection import MqttNamespaceConnection
 from .services.service import Service
