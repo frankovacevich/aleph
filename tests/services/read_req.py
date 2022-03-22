88cc925ec654f0b9f2aba670eef1192d781eb682
Me <>
2022-03-22 07:23:25 -0300

    Created test

--- tests/services/read_req.py
+++ tests/services/read_req.py
@@ -1,3 +1,4 @@
+
 from aleph.connections.namespace.mqtt_namespace_connection import MqttNamespaceConnection
 from aleph.connections.testing.random import RandomConnection
 from aleph.services.mqtt import MqttService
@@ -10,6 +11,15 @@ class GenericService(MqttService):
 
     def __init__(self):
         super().__init__()
+
+    def on_status_change(self, status_code):
+        print("STATUS CHANGED", status_code)
+
+    def on_read_request(self, key, **kwargs):
+        print("NEW READ REQUEST", key, kwargs)
+        response = super().on_read_request(key, **kwargs)
+        print("RESPONSE", response)
+        return response
 
 
 if __name__ == "__main__":
