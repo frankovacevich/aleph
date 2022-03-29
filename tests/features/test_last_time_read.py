ed641506f407dc1bc38f796248205c9c8071e0e5
Me <>
2022-03-29 06:10:37 -0300

    Updated

--- tests/features/test_last_time_read.py
+++ tests/features/test_last_time_read.py
@@ -1,6 +1,7 @@
 from aleph import Connection
 import datetime
 import time
+from aleph.common.datetime_functions import now
 
 
 class TestConn(Connection):
@@ -13,6 +14,9 @@ class TestConn(Connection):
         {"x": 5, "t": t0 + datetime.timedelta(seconds=5)},
         {"x": 6, "t": t0 + datetime.timedelta(seconds=10)},
         {"x": 7, "t": t0 + datetime.timedelta(seconds=15)},
+        {"x": 7.1, "t": t0 + datetime.timedelta(seconds=15.1)},
+        {"x": 7.2, "t": t0 + datetime.timedelta(seconds=15.2)},
+        {"x": 7.3, "t": t0 + datetime.timedelta(seconds=16)},
         {"x": 8, "t": t0 + datetime.timedelta(seconds=20)},
         {"x": 9, "t": t0 + datetime.timedelta(seconds=25)},
         {"x": 10, "t": t0 + datetime.timedelta(seconds=30)},
@@ -21,13 +25,13 @@ class TestConn(Connection):
     def read(self, key, **kwargs):
 
         print(kwargs["since"], kwargs["until"])
+        return [x.copy() for x in self.data if x["t"] <= datetime.datetime.now()]
 
-        return self.data
-
+    def on_read_error(self, error):
+        raise
 
 T = TestConn()
-for t in T.data:
-    print(t["x"], t["t"])
+for t in T.data: print(t["x"], t["t"])
 
 
 time.sleep(2)
