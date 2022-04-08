967f5f2867369cbb659d53f6f8727b7d07b5d59a
Me <>
2022-04-08 18:23:04 -0300

    Fixed bug

--- tests/features/test_wait_one_step.py
+++ tests/features/test_wait_one_step.py
@@ -12,14 +12,12 @@ class TestWaitOneStep(unittest.TestCase):
             w.wait()
             print(time.time(), time.time() - t0)
 
-    """
     def test2(self):
         t0 = time.time()
         w = WaitOneStep(60)
         for i in range(0, 5):
             w.wait()
             print(time.time() - t0)
-    """
 
 
 if __name__ == '__main__':
