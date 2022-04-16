3b7b74fdc8b098bb66d7c3c37614f0ced3516053
Me <>
2022-04-16 12:17:42 -0300

    Removed unused function

--- aleph/common/dict_functions.py
+++ aleph/common/dict_functions.py
@@ -36,24 +36,3 @@ def unflatten_dict(dictionary):
             d = d[part]
         d[parts[-1]] = value
     return result
-
-
-def map_dict_keys(list_of_dict, key_map):
-    """
-    Takes a list of dictionaries and changes the key of each dict to a new set of keys,
-    given by key_map = {old_key: new_key}. It returns an iterable.
-
-    For example:
-    > L = [{"A": 1, "B": 2, "C": 3}, {"A": 4, "B": 5, "C": 6}, {"A": 7, "B": 8, "C": 9}]
-    > m = {"A": "aa", "B": "bb", "C": "cc"}
-    > Y = list(map_dict_keys(L, m))
-    > [{"aa": 1, "bb": 2, "cc": 3}, {"aa": 4, "bb": 5, "cc": 6}, {"aa": 7, "bb": 8, "cc": 9}]
-    """
-
-    def key_map_fun(dictionary):
-        d = dictionary.copy()
-        for k in key_map:
-            d[key_map[k]] = d.pop(k, None)
-        return d
-
-    return map(key_map_fun, list_of_dict)
