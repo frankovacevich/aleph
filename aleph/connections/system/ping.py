ecb0492cb8c352612a8581f786b9214775d435ff
Me <>
2022-04-22 12:08:05 -0300

    Refactored attribute names

--- aleph/connections/system/ping.py
+++ aleph/connections/system/ping.py
@@ -8,13 +8,12 @@ class PingConnection(Connection):
 
     def __init__(self, client_id=""):
         super().__init__(client_id)
-
-        # Connection parameters
+        self.multi_threaded = True
         self.clean_on_read = False
         self.report_by_exception = True
 
         # Other options
-        self.consecutive_pings_to_declare_dead = 2
+        self.consecutive_pings_to_declare_offline = 2
 
         # Internal
         self.__ping_count__ = {}
@@ -25,6 +24,8 @@ class PingConnection(Connection):
         The key is the IP address "X.X.X.X"
         You can also check if a port is open using "X.X.X.X:PORT"
         """
+
+        # Get ping status (r = True or False)
         if ":" in key:
             k = key.split(":")
             r = self.check_port(k[0], int(k[1]))
@@ -33,12 +34,13 @@ class PingConnection(Connection):
             r = self.ping(key)
             key = key.replace(".", "_")
 
+        # Determine result based on ping count
         if r or key not in self.__ping_count__:
             self.__ping_count__[key] = 0
         else:
             self.__ping_count__[key] += 1
-            if self.__ping_count__[key] >= self.consecutive_pings_to_declare_dead:
-                self.__ping_count__[key] = self.consecutive_pings_to_declare_dead
+            if self.__ping_count__[key] >= self.consecutive_pings_to_declare_offline:
+                self.__ping_count__[key] = self.consecutive_pings_to_declare_offline
                 r = False
             else:
                 r = True
